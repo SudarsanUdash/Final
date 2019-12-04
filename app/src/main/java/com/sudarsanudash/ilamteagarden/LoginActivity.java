73dd20f4e54d644af8fdd15046a3c8dadf7a6bfb
Sudarsan Udash <sudremotae@gmail.com>
2019-12-04 08:05:43 +0545

    Layouts for Registration and Login System Created

--- app/src/main/java/com/sudarsanudash/ilamteagarden/LoginActivity.java
+++ app/src/main/java/com/sudarsanudash/ilamteagarden/LoginActivity.java
@@ -1,14 +1,87 @@
 package com.sudarsanudash.ilamteagarden;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText et_Email, et_password;
+    Button btn_login;
+
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().setTitle("IlamTeaGarden");
+
+
+       et_Email = (EditText)findViewById(R.id.et_Email);
+       et_password = (EditText)findViewById(R.id.et_password);
+       btn_login = (Button)findViewById(R.id.btn_login);
+
+       firebaseAuth = FirebaseAuth.getInstance();
+
+       btn_login.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+               String Email = et_Email.getText().toString().trim();
+
+               String password = et_password.getText().toString().trim();
+
+               if (TextUtils.isEmpty(Email)) {
+                   Toast.makeText(LoginActivity.this, "Please Enter Your Email", Toast.LENGTH_SHORT).show();
+                   return;
+               }
+               if (TextUtils.isEmpty(password)) {
+                   Toast.makeText(LoginActivity.this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+                   return;
+               }
+
+               if (password.length()<6){
+                   Toast.makeText(LoginActivity.this, "Password is too short", Toast.LENGTH_SHORT).show();
+               }
+
+               firebaseAuth.signInWithEmailAndPassword(Email, password)
+                       .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                           @Override
+                           public void onComplete(@NonNull Task<AuthResult> task) {
+                               if (task.isSuccessful()) {
+                                   startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                   Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                               } else {
+                                   Toast.makeText(LoginActivity.this, "Cannot Login Some Credentials are incorrect", Toast.LENGTH_SHORT).show();
+
+                               }
+
+                               // ...
+                           }
+                       });
+
+
+
+           }
+       });
+
+
+    }
+
+    public void btn_SignupForm(View view) {
+        startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
     }
 }
---
+++ app/src/main/java/com/sudarsanudash/ilamteagarden/SignUpActivity.java
@@ -0,0 +1,117 @@
+package com.sudarsanudash.ilamteagarden;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText et_FullName, et_PhoneNumber, et_Address, et_Email, et_password, et_cpassword;
+    Button btn_register, btn_login;
+    ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        et_FullName = (EditText) findViewById(R.id.et_FullName);
+        et_PhoneNumber = (EditText) findViewById(R.id.et_PhoneNumber);
+        et_Address = (EditText) findViewById(R.id.et_Address);
+        et_Email = (EditText) findViewById(R.id.et_Email);
+        et_password = (EditText) findViewById(R.id.et_password);
+        et_cpassword = (EditText) findViewById(R.id.et_cpassword);
+        btn_register = (Button) findViewById(R.id.btn_register);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                String FullName = et_FullName.getText().toString().trim();
+                                                String PhoneNumber = et_PhoneNumber.getText().toString().trim();
+                                                String Address = et_Address.getText().toString().trim();
+                                                String Email = et_Email.getText().toString().trim();
+                                                String password = et_password.getText().toString().trim();
+                                                String cpassword = et_cpassword.getText().toString().trim();
+
+                                                if (TextUtils.isEmpty(FullName)) {
+                                                    Toast.makeText(SignUpActivity.this, "Please Enter Your Full Name", Toast.LENGTH_SHORT).show();
+                                                    return;
+                                                }
+                                                if (TextUtils.isEmpty(PhoneNumber)) {
+                                                    Toast.makeText(SignUpActivity.this, "Please Enter Phone Number", Toast.LENGTH_SHORT).show();
+                                                    return;
+                                                }
+                                                if (TextUtils.isEmpty(Email)) {
+                                                    Toast.makeText(SignUpActivity.this, "Please Enter Your Email", Toast.LENGTH_SHORT).show();
+                                                    return;
+                                                }
+                                                if (TextUtils.isEmpty(Address)) {
+                                                    Toast.makeText(SignUpActivity.this, "Please Enter Your Address", Toast.LENGTH_SHORT).show();
+                                                    return;
+                                                }
+                                                if (TextUtils.isEmpty(password)) {
+                                                    Toast.makeText(SignUpActivity.this, "Please Enter Your password", Toast.LENGTH_SHORT).show();
+                                                    return;
+                                                }
+                                                if (TextUtils.isEmpty(cpassword)) {
+                                                    Toast.makeText(SignUpActivity.this, "Please confirm your password", Toast.LENGTH_SHORT).show();
+                                                    return;
+                                                }
+
+                                                if (password.length()<6){
+                                                    Toast.makeText(SignUpActivity.this, "Password is too short", Toast.LENGTH_SHORT).show();
+                                                }
+                                                progressBar.setVisibility(View.VISIBLE);
+
+                                                if (password.equals(cpassword)){
+                                                    firebaseAuth.createUserWithEmailAndPassword(Email, password)
+                                                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                                                    progressBar.setVisibility(View.GONE);
+                                                                    if (task.isSuccessful()) {
+                                                                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                                                        Toast.makeText(SignUpActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+
+
+                                                                    } else {
+                                                                        Toast.makeText(SignUpActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+
+
+                                                                    }
+
+                                                                    // ...
+                                                                }
+                                                            });
+
+                                                }
+                                            }
+                                        });
+
+
+
+
+    }
+    public void btn_LoginForm(View view) {
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+    }};
